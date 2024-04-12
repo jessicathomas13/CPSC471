3f626b191f175c689c2a5fc0d303b0b6b42fffd1
Jessica Thomas <127780039+jessicathomas13@users.noreply.github.com>
2024-04-11 22:38:20 -0600

    Update admin-event.php

--- includes/admin-event.php
+++ includes/admin-event.php
@@ -8,98 +8,98 @@ include('sqlconnect.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title> Online Library Management System </title>
-  <link rel="stylesheet" href="assets/bootstrap.css">
-  <link rel="stylesheet" href="assets/font-awesome.css">
-  <style>
-    .lel {
-      border: 1px solid black;
-      border-radius: 5px;
-    }
-  </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Management</title>
+    <link rel="stylesheet" href="assets/bootstrap.css">
+    <link rel="stylesheet" href="assets/font-awesome.css">
+    <script>
+        function toggleDelete(shouldShow) {
+            const checkboxes = document.querySelectorAll('.delete-checkbox');
+            const deleteBtn = document.getElementById('delete-btn');
+            checkboxes.forEach(cb => cb.style.display = shouldShow ? 'block' : 'none');
+            deleteBtn.style.display = shouldShow ? 'block' : 'none';
+        }
+
+        function confirmDeletion() {
+            const checkedBoxes = document.querySelectorAll('.delete-checkbox:checked');
+            return checkedBoxes.length > 0 ? confirm('Are you sure you want to delete the selected events?') : false;
+        }
+    </script>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding-top: 60px;
+      }
+      .navbar {
+        background-color: #333;
+        overflow: hidden;
+        position: fixed;
+        top: 0;
+        width: 100%;
+        height: 50px;
+        box-shadow: 0 2px 4px rgba(0,0,0,0.4);
+      }
+      .navbar h4, .navbar a {
+        float: right;
+        display: block;
+        color: #f2f2f2;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        transition: background-color 0.3s;
+      }
+      .navbar h4:hover, .navbar a:hover {
+        background-color: #ddd;
+        color: black;
+      }
+      .navbar h1 {
+        float: left;
+        color: #f2f2f2;
+        text-align: center;
+        padding: 10px 16px;
+        font-size: 24px;
+        margin: 0;
+      }
+      .delete-checkbox, #delete-btn {
+            display: none; /* Hide checkboxes and delete button by default */
+            margin-top: 10px;
+        }
+    </style>
 </head>
-
 <body>
-<nav class="navbar">
+    <nav class="navbar">
+        <h1>Events</h1>
+        <a href="admin-dashboard.php">Home</a>
+        <a href="#" onclick="toggleDelete(true)">Delete Event</a>
+        <a href="add-event.php">Add Event</a>
+    </nav>
 
-<?php // var_dump($_SERVER["PHP_SELF"]);  ?>  <!-- this will show your url -->
-<div class="content-wrapper">
-         <div class="container">
-        <div class="row pad-botm">
-            <div class="col-md-30">
-              <div style = "text-align: right">
-              <h4 class="header-line"><a class="default-link-style <?php if($_SERVER["PHP_SELF"]=='add-book.php'){echo 'style-active';}?>"  href="add-book.php">Add Book</a></h4> 
-              <h4 class="header-line"><a class="default-link-style <?php if($_SERVER["PHP_SELF"]=='delete-book.php'){echo 'style-active';}?>"  href="delete-book.php">Delete Book</a></h4>
-              <h4 class="header-line"><a class="default-link-style <?php if($_SERVER["PHP_SELF"]=='admin-login.php'){echo 'style-active';}?>"  href="admin-login.php">Logout</a></h4>
-              <h4 class="header-line"><a class="default-link-style <?php if($_SERVER["PHP_SELF"]=='all-admins.php'){echo 'style-active';}?>"  href="all-admins.php">OUR ADMINS</a></h4>
-              <h4 class="header-line"><a class="default-link-style <?php if($_SERVER["PHP_SELF"]=='event.php'){echo 'style-active';}?>"  href="add-event.php">ADD EVENT</a></h4>
-              <h4 class="header-line"><a class="default-link-style <?php if($_SERVER["PHP_SELF"]=='publisher.php'){echo 'style-active';}?>"  href="publisher.php">PUBLISHERS</a></h4>
-            </div>
-
-</nav>
-
-<style>
-
-.style-active {
-        text-decoration: red underline overline wavy;
-}
-
-</style>
-<div class="content-wrapper">
-         <div class="container">
-        <div class="row pad-botm">
-            <div class="col-md-12">
-                <h4 class="header-line">EVENTS</h4>
-    </div>
-    
-
-            <div class="row">
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row pad-botm">
                 <div class="col-md-12">
-                    <!-- Advanced Tables -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                          All Events 
-                        </div>
-                        <div class="panel-body">
-                       
-
-<?php  
-$sql = "SELECT * FROM event";
-
-$result = mysqli_query($con, $sql);
-
-                                
-  
-while ($row = mysqli_fetch_assoc($result))  {             ?>  
-<div class="col-md-4" style="float:left; height:300px;">   
-
-                                     
-
-                                                <br /><b><?php echo $row['Date'];?></b><br />
-                                                <?php echo $row['Description'];?><br />
-                                            
-                                                
+                    <form method="POST" action="delete-event.php" onsubmit="return confirmDeletion();">
+                      <?php
+                        $sql = "SELECT * FROM event";
+                        $result = mysqli_query($con, $sql);
+                        
+                        while ($row = mysqli_fetch_assoc($result)) { ?>
+                            <div class="col-md-4" style="float:left; height:300px;">
+                                <b><?php echo $row['EventID'];?></b><br />
+                                <?php echo $row['BranchID'];?><br />
+                                <?php echo $row['Date'];?><br />
+                                <?php echo $row['Description'];?><br />
+                                <input type="checkbox" class="delete-checkbox" name="delete_ids[]" value="<?php echo $row['ID']; ?>"><br>
                             </div>
-
-                                <?php } ?>  
-                      
-                            
-                        </div>
-                    </div>
-                    <!--End Advanced Tables -->
+                        <?php } ?>
+                        <button type="submit" id="delete-btn" class="btn btn-danger">Delete Selected</button>
+                    </form>
                 </div>
             </div>
-
-
-            
+        </div>
     </div>
-    </div>
-    </div>
-
 </body>
-
 </html>
-
